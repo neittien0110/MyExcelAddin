2a07b5f8b8fad6f79298445e5fcc7fcdede1c403
Nguyễn Đức Tiến <neittien0110@gmail.com>
2020-10-11 22:34:49 +0700

    Thêm hàm tính URL Sharepoint Profile Fix-bug: MSSV không đủ 8 chữ số

--- ExcelFuntions/Hust.cs
+++ ExcelFuntions/Hust.cs
@@ -21,14 +21,26 @@ namespace MyExcelAddIn
         /// <param name="MaSoSinhVien">Mã số SV do trường cấp. Ví dụ 20002987</param>
         /// <returns></returns>
         /// <remarks> ExcelDna.Integration.ExcelFunction(Name = ...)  sẽ qui định tên hàm để gọi ra trong Excel </remarks>
-        [ExcelDna.Integration.ExcelFunction(Description = "Tính địa chỉ email HUST của sinh viên dựa theo tên và mã số sinh viên", Name = "EmailSinhVien", Category = "Text")]
+        [ExcelDna.Integration.ExcelFunction(Description = "Tính địa chỉ email HUST của sinh viên, giảng viên dựa theo tên và mã số sinh viên", Name = "EmailSinhVien", Category = "Text")]
         public static object StudentEmail(
-            [ExcelDna.Integration.ExcelArgument(Description ="Họ và tên đầy đủ. Ví dụ Đinh Công Thuật")]  string HoVaTen,  
-            [ExcelDna.Integration.ExcelArgument(Description ="Mã số SV do trường cấp. Ví dụ 20002987")]   string MaSoSinhVien)
+            [ExcelDna.Integration.ExcelArgument(Description = "Họ và tên đầy đủ. Ví dụ Đinh Công Thuật")] string HoVaTen,
+            [ExcelDna.Integration.ExcelArgument(Description = "Mã số SV do trường cấp. Bỏ trống nếu là giảng viên. Ví dụ 20002987.")] string MaSoSinhVien)
         {
             string TenKhongDau = NameTools.LayTenKhongDau(HoVaTen);
-            string ChuCaiDau = NameTools.LayCacChuCaiDau(TenKhongDau);
-            string Email = NameTools.LayTen(TenKhongDau) + "." + ChuCaiDau.Substring(0, ChuCaiDau.Length - 1) + MaSoSinhVien.Substring(2, MaSoSinhVien.Length - 2) + "@sis.hust.edu.vn";
+            string Email;
+            if (MaSoSinhVien.Length == 8)
+            {
+                string ChuCaiDau = NameTools.LayCacChuCaiDau(TenKhongDau);
+                Email = NameTools.LayTen(TenKhongDau) + "." + ChuCaiDau.Substring(0, ChuCaiDau.Length - 1) + MaSoSinhVien.Substring(2, MaSoSinhVien.Length - 2) + "@sis.hust.edu.vn";
+            }
+            else if (MaSoSinhVien.Length == 0)
+            {
+                Email = NameTools.LayTen(TenKhongDau) + "." + NameTools.LayHo(TenKhongDau) + NameTools.LayDem(TenKhongDau).Trim() + "@hust.edu.vn";
+            }
+            else
+            {
+                Email = "Mã số không hợp lệ";
+            }    
             return Email;
         }
 
@@ -39,7 +51,7 @@ namespace MyExcelAddIn
         /// <returns></returns>
         /// <remarks> Hàm async, cho phép trả về dữ liệu chậm hơn </remarks>
         [ExcelDna.Integration.ExcelFunction(Description = "Tìm địa chỉ email HUST của sinh viên dựa theo tên và mã số sinh viên", Name = "EmailSinhVien2", Category = "Text")]
-        public static object StudentEmail2(string HoVaTen, string MaSoSinhVien)
+        public static object JustForFun(string HoVaTen, string MaSoSinhVien)
         {
             return ExcelAsyncUtil.Run("RunSomethingDelay", new[] { HoVaTen, MaSoSinhVien }, () => RunSomethingDelay(HoVaTen));
         }
@@ -49,5 +61,22 @@ namespace MyExcelAddIn
             Thread.Sleep(1000);
             return $"Hello {name}";
         }
+
+        /// <summary>
+        /// Students the profile.
+        /// </summary>
+        /// <param name="HoVaTen">The ho va ten.</param>
+        /// <param name="MaSoSinhVien">The ma so sinh vien.</param>
+        /// <returns>
+        ///     https://husteduvn-my.sharepoint.com/person.aspx?user=tuong.pd164582@sis.hust.edu.vn
+        /// </returns>
+        [ExcelDna.Integration.ExcelFunction(Description = "Đường link thông tin cá nhân công khai của email HUST, bao gồm cả giáo viên và sinh viên. ", Category = "Text")]
+        public static object SharepointProfile(
+         [ExcelDna.Integration.ExcelArgument(Description = "Email HUST của sinh viên. Ví dụ chinh.bq002987@sis.hust.edu.vn")] string EmailSinhVien)
+        {
+            return $"https://husteduvn-my.sharepoint.com/person.aspx?user={EmailSinhVien}";
+        }
+        
+
     }
 }
