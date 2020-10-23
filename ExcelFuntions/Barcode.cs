e7dfd339d6025838350cc31b23e1856ac4bdbdff
Tuong Phan <tramphongsts@gmail.com>
2020-10-23 12:08:38 +0700

    ZXing using temp file

--- ExcelFuntions/Barcode.cs
+++ ExcelFuntions/Barcode.cs
@@ -47,8 +47,35 @@ namespace MyExcelAddIn
             return sURL.ToString();
         }
 
+        /// <summary>
+        ///         Trả về file name (path) của ảnh tạo ra bởi ZXing library (temp file)
+        /// </summary>
+        /// <param name="Text">Văn bản cần sinh mã QR</param>
+        /// <param name="ImageSize">Kích thước của ảnh QR. Tối đa là 500 px</param>
+        /// <param name="Correction">Mức độ chịu lỗi</param>
+        /// <param name="Margin">Số điểm ảnh trắng để làm biên </param>
+        /// <returns></returns>
+        static string GetQRCodeLocalFileNameByZXing(string Text, int ImageSize = 500, CorrectionLevel Correction = CorrectionLevel.High, int Margin = 0)
+        {
+            QRCodeWriter qr = new ZXing.QrCode.QRCodeWriter(); //QRCode as a BitMatrix 2D array
+
+            Dictionary<EncodeHintType, object> hint = new Dictionary<EncodeHintType, object>();
+            hint.Add(EncodeHintType.MARGIN, Margin); // margin of the QRCode image
+            hint.Add(EncodeHintType.ERROR_CORRECTION, Correction);
+
+            var matrix = qr.encode(Text, BarcodeFormat.QR_CODE, ImageSize, ImageSize, hint); // encode QRCode matrix from source text
+            ZXing.BarcodeWriter w = new ZXing.BarcodeWriter();
+            Bitmap img = w.Write(matrix); // QRCode Bitmap image
+            string tempFile = Path.GetTempFileName(); //create a temp file to save QRCode image
+            img.Save(tempFile, System.Drawing.Imaging.ImageFormat.Png);//save QRCode image to temp file
+
+            return tempFile;
+        }
+
         [ExcelDna.Integration.ExcelFunction(Description = "Tạo mã QRCode")]
         public static object QRCode(
+            [ExcelDna.Integration.ExcelArgument(Description = "Dịch vụ tạo QRCode; 1 = ZXing library, 2 = Google API")]
+            int option,
             [ExcelDna.Integration.ExcelArgument(Description = "Tên của Shape sẽ chứa ảnh QRCode (xem bằng Selection Pane). Nếu shape chưa tồn tại, hàm sẽ tự tạo mới. Ví dụ: tl123")] 
             string ShapeName,
             [ExcelDna.Integration.ExcelArgument(Description = "Văn bản cần chuyển thành QRcode. Ví dụ: xin chào bạn")]
@@ -85,7 +112,15 @@ namespace MyExcelAddIn
             {
                 try
                 {
-                    MyShape.Fill.UserPicture(Barcode.GetQRCodeWebAPI(Text, 500, CorrectionLevel.High, Margin));
+                    if (option == 1)
+                    {
+                        MyShape.Fill.UserPicture(Barcode.GetQRCodeLocalFileNameByZXing(Text, 500, CorrectionLevel.High, Margin));
+                    } 
+                    else if (option == 2)
+                    {
+                        MyShape.Fill.UserPicture(Barcode.GetQRCodeWebAPI(Text, 500, CorrectionLevel.High, Margin));
+                    }
+                    
                 }
                 catch
                 {
@@ -95,47 +130,45 @@ namespace MyExcelAddIn
             return Text;
         }
 
+        // Old codes before refactor
         //Generate QRCode using ZXing library
-        [ExcelDna.Integration.ExcelFunction(Description = "QRCode generator by ZXing lib")]
-        public static object QRCodeZ(
-            [ExcelDna.Integration.ExcelArgument(Description = "Text to be transformed to QRCode. Example: \"hello\"")] 
-            string Text)
-        {
-            //string imageFile = "C:\\Users\\ASUS\\Desktop\\myQR.png"; // image file to save as QRCode image, change path in different devices
-            QRCodeWriter qr = new ZXing.QrCode.QRCodeWriter(); //QRCode as a BitMatrix 2D array
+        //[ExcelDna.Integration.ExcelFunction(Description = "QRCode generator by ZXing lib")]
+        //public static object QRCodeZ(
+        //    [ExcelDna.Integration.ExcelArgument(Description = "Shape name to contain QRCode image (view Selection Pane). If not existed, new shape will be created. Example: tl123")]
+        //    string ShapeName,
+        //    [ExcelDna.Integration.ExcelArgument(Description = "Text to be transformed to QRCode. Example: \"hello\"")] 
+        //    string Text)
+        //{
+        //    Application xlApp = (Application)ExcelDnaUtil.Application;
+        //    Workbook wb = xlApp.ActiveWorkbook;
+        //    if (wb == null) return "";
+        //    Worksheet ws = wb.ActiveSheet;
 
-            Dictionary<EncodeHintType, object> hint = new Dictionary<EncodeHintType, object>();
-            hint.Add(EncodeHintType.MARGIN, 0); // margin of the QRCode image
+        //    QRCodeWriter qr = new ZXing.QrCode.QRCodeWriter(); //QRCode as a BitMatrix 2D array
+
+        //    Dictionary<EncodeHintType, object> hint = new Dictionary<EncodeHintType, object>();
+        //    hint.Add(EncodeHintType.MARGIN, 0); // margin of the QRCode image
 
 
-            var matrix = qr.encode(Text, BarcodeFormat.QR_CODE, 350, 350, hint); // encode QRCode matrix from source text
+        //    var matrix = qr.encode(Text, BarcodeFormat.QR_CODE, 50, 50, hint); // encode QRCode matrix from source text
             
-            ZXing.BarcodeWriter w = new ZXing.BarcodeWriter();
-            Bitmap img = w.Write(matrix); // QRCode Bitmap image
-            //img.Save(@imageFile, System.Drawing.Imaging.ImageFormat.Png); // save QRCode image to local device
-            
-            MemoryStream ms = new MemoryStream(); 
-            img.Save(ms, System.Drawing.Imaging.ImageFormat.Png); //save QRCode image to memory stream
-            System.Drawing.Image i = System.Drawing.Image.FromStream(ms); // create image in Image form
-            
-            Application xlApp = (Application)ExcelDnaUtil.Application;
-            Workbook wb = xlApp.ActiveWorkbook;
-            if (wb == null) return "";
-            Worksheet ws = wb.ActiveSheet;
+        //    ZXing.BarcodeWriter w = new ZXing.BarcodeWriter();
+        //    Bitmap img = w.Write(matrix); // QRCode Bitmap image
+        //    string tempFile = Path.GetTempFileName();
 
-            //the following codes took me hours, just leave them here for references
-            //add QRCode image to this worksheet
-            //ws.Shapes.AddPicture(@imageFile, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoCTrue, 0, 0, 185, 185);
-            //Shape myShape = ws.Shapes.AddShape(Microsoft.Office.Core.MsoAutoShapeType.msoShapeRectangle, xlApp.ActiveCell.Left, xlApp.ActiveCell.Top, xlApp.ActiveCell.Width, xlApp.ActiveCell.Height);
-            //System.Drawing.Image.FromStream(ms);
-            //myShape.Fill.UserPicture(imageMemoryStream.ToString());
-            //ws.
-            //ws.Shapes.AddPicture(i, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoCTrue, 0, 0, 185, 185);
+        //    img.Save(tempFile, System.Drawing.Imaging.ImageFormat.Png);
+
+
+        //    Shape MyShape = null;
+
+           
+        //    MyShape = ws.Shapes.AddShape(Microsoft.Office.Core.MsoAutoShapeType.msoShapeRectangle, xlApp.ActiveCell.Left, xlApp.ActiveCell.Top, xlApp.ActiveCell.Width, xlApp.ActiveCell.Height);
             
-            Clipboard.SetDataObject(i); //set image to clipboard
-            ws.Paste(); //just paste the clipboard
             
-            return Text;
-        }
+        //    MyShape.Fill.UserPicture(tempFile);
+            
+            
+        //    return Text;
+        //}
     }
 }
